ebf95d49443eb0b3a693db6e02c0ae212b88b9af
Hawraa Hijazi <hawraahijazi1996@gmail.com>
2025-01-25 19:07:09 +0000

    connection the postgres to python

---
+++ sql-psycopg2.py
@@ -0,0 +1,25 @@
+import psycopg2
+
+
+# connect to "chinook" database
+connection = psycopg2.connect(database ="chinook")
+
+
+# build a cursor object of the database
+cursor = connection.cursor()
+cursor.execute('SELECT * FROM "Artist"')
+
+# fetch the results (multiple)
+results = cursor.fetchall()
+
+
+# fetch the results (single)
+# results = cursor.fetchone()
+
+# close the connection 
+connection.close()
+
+
+# print results
+for result in results:
+    print(result)
